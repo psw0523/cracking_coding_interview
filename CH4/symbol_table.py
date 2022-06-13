0030b2f0e3c8dc0d2058db280d6ca9e64d6d1867
sungwpark <psw0523@gmail.com>
2022-06-13 21:34:23 +0900

    Add CH4 and symbol table API

---
+++ CH4/symbol_table.py
@@ -0,0 +1,24 @@
+class SymbolTable():
+    def __init__(self):
+        pass
+    
+    def get(self, key):
+        pass
+
+    def put(self, key, value):
+        pass
+
+    def delete(self, key):
+        pass
+
+    def contains(self, key):
+        pass
+
+    def size(self):
+        pass
+
+    def is_empty(self):
+        pass
+
+    def keys(self):
+        pass
