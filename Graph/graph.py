a0b36c25b21050ba8e98f8eaa3c210e7e591255a
sungwpark <psw0523@gmail.com>
2022-06-20 22:19:27 +0900

    Add WeightedDirectedGraph

--- Graph/graph.py
+++ Graph/graph.py
@@ -53,6 +53,60 @@ class DirectedGraph(Graph):
         self.adj[v-1].append(w)
 
 
+
+class Edge():
+    def __init__(self, v, w, weight):
+        self.v = v
+        self.w = w
+        self.weight = weight
+
+
+    def __str__(self):
+        l = [str(self.v), "->", str(self.w), ", w ", str(self.weight)]
+        return "".join(l)
+
+
+    def __repr__(self):
+        l = [str(self.v), "->", str(self.w), " w ", str(self.weight)]
+        return "".join(l)
+
+
+
+class WeightedDirectedGraph(Graph):
+    def init_from_file(self, file_name):
+        try:
+            f = open(file_name, "r")
+        except e as error:
+            print(error)
+            return 0
+
+        lines = f.readlines()
+        line0_split = lines[0].split()
+        self.V = int(line0_split[0])
+        self.E = int(line0_split[1])
+        self.adj = list()
+        for i in range(self.V):
+            self.adj.append(list())
+
+        for i in range(1, len(lines)):
+            e = lines[i].split()
+            v = int(e[0])
+            w = int(e[1])
+            weight = int(e[2])
+            edge = Edge(v, w, weight)
+            self.add_edge(edge)
+
+
+    def add_edge(self, e):
+        self.adj[e.v-1].append(e)
+
+
+    def __str__(self):
+        return str(self.adj)
+
+
+
+
 if __name__ == '__main__':
     graph = Graph("g.txt")
     print(graph)
@@ -60,3 +114,5 @@ if __name__ == '__main__':
     print(directed_graph)
     directed_graph2 = DirectedGraph("dg2.txt")
     print(directed_graph2)
+    weighted_directed_graph = WeightedDirectedGraph("wdg.txt")
+    print(weighted_directed_graph)
