32082ad1502ddc294e34d64c84152e53d848d137
sungwpark <psw0523@gmail.com>
2022-06-26 21:41:20 +0900

    Add TODOs

---
+++ easy/TODO-rectangle-overlap.py
@@ -0,0 +1,15 @@
+'''
+Problem
+Asked by Google.
+
+You are given a list of rectangles representing by min and max x- and y- coordinates.
+Compute whether or not a pair of rectangles overlap each other.
+If one rectangle completely covers another, it is considered overlapping.
+
+For example, given the following rectangles:
+    (1,4)  (3,3)
+    (-1,3) (2,1)
+    (0,5)  (4,3)
+
+return true as the first and third rectangle overlap each other.
+'''
